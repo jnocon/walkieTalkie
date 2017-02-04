6adb568bcbbac37cdbffde756e401066da0f8e4c
menaduke <david1991park@gmail.com>
2017-02-04 10:10:39 -0800

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-// rakon db mysql://admin:VJRRJJDHOFSKAPWL@aws-us-east-1-portal.5.dblayer.com:16918/compose
-
-var db = new Sequelize('mysql://admin:DRCEOPOGMXRLKTZP@aws-us-east-1-portal.7.dblayer.com:15853/compose');
+// OURs db mysql://admin:VJRRJJDHOFSKAPWL@aws-us-east-1-portal.5.dblayer.com:16918/compose
+//ACTUALLY RAKONS mysql://admin:DRCEOPOGMXRLKTZP@aws-us-east-1-portal.7.dblayer.com:15853/compose
+var db = new Sequelize('mysql://admin:VJRRJJDHOFSKAPWL@aws-us-east-1-portal.5.dblayer.com:16918/compose');
 module.exports = db;
